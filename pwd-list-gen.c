4de1f053cba31bd767fa15e0ccdb3f732463c65a
Elliott Sobek <sobeke@mymacewan.ca>
2017-06-22 18:17:34 -0600

    Fix backwards checking logic for reversal on array

--- pwd-list-gen.c
+++ pwd-list-gen.c
@@ -5,7 +5,7 @@ int main(void) {
 	const char * const choices = "abc";
 	char entry[4] = {'0', '0', '0', '\0'};
 	int len_n = strlen(choices);
-	char const last_elem = choices[len_n - 1];
+	const char last_elem = choices[len_n - 1];
 
 	for (int i = 0; i < len_n; i++) // Init string/entry
 		entry[i] = choices[0]; // "aaa"
@@ -13,7 +13,9 @@ int main(void) {
 	printf("Entry: %s\n", entry); // TMP
 	printf("This is strlen: %d\n", len_n); // TMP, 3
 
-	while (strncmp(entry, "ccc", 3) != 0) { // While entry not last entry
+	int b = 0;
+
+	while (strncmp(entry, "ccc", 3) != 0 && b != 3) { // While entry not last entry
 
 		for (int i = len_n - 1; i > -1; i--) { // Go back down the entry list from the back
 
@@ -23,14 +25,20 @@ int main(void) {
 
 				for (int j = i; j < len_n; j++) // Reset current index and forward ones to base choice
 					entry[j] = choices[0];
+			} else {
+				printf("%s\n", entry);
+				entry[i] = choices[len_n - i]; // Increment current index choice to the next one
+				break;
 			}
-			printf("%s\n", entry);
-			entry[i] = choices[len_n - i]; // Increment current index choice to the next one
 		}
-
-		break; // TMP
-
+		b++;
 	}
 
 	return 0;
 }
+
+/* If entry[i] == last_elem
+else
+break
+make current index elem goto next choice
+*/
