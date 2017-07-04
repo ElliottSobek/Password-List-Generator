9a76e75c548b11d30e24adc497fd2c68dcf08165
Elliott Sobek <sobeke@mymacewan.ca>
2017-07-04 02:35:04 -0600

    Complete initial implementation of all legth choices combinations

--- pwd-list-gen.c
+++ pwd-list-gen.c
@@ -1,3 +1,5 @@
+/**/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -14,6 +16,7 @@ while (getopt(argc, argv, "hp:c:") != 0) {
 		case 'c':
 			break
 		default:
+			fprintf(stderr, "Error\n");
 			exit(EXIT_FAILURE);
 	}
 }
@@ -31,7 +34,9 @@ char get_next_char(const char c) {
 	const char * const choices = "abc";
 
 	for (int i = 0; i < 3; i++)
-		if (c == choices[i])
+		if (c == 'c')
+			return choices[0];
+		else if (c == choices[i])
 			return choices[i + 1];
 	return '\0';
 }
@@ -42,39 +47,58 @@ int main(void) {
 	const char last_elem = choices[len_n - 1];
 	char entry[4] = {'0', '0', '0', '\0'}, end_entry[4] = {'0', '0', '0', '\0'};
 
-	for (int i = 0; i < len_n; i++) // Init string/entry
+	for (int i = 0; i < len_n; i++) { // Init string/entry
 		entry[i] = choices[0]; // "aaa"
-	for (int i = 0; i < len_n; i++)
-		end_entry[i] = last_elem;
+		end_entry[i] = last_elem; // "ccc"
+	}
 
-	printf("Entry: %s\n", entry); // TMP
-	printf("This is strlen: %d\n", len_n); // TMP, 3
-	printf("This is end_entry: %s\n\n", end_entry); // TMP
+	// printf("Entry: %s\n", entry); // TMP
+	// printf("This is strlen: %d\n", len_n); // TMP, 3
+	// printf("This is end_entry: %s\n\n", end_entry); // TMP
 
 	int b = 0;
 
-	while (strncmp(entry, end_entry, 3) != 0 && b != 9) { // While entry not last entry
+	// entry[0] = 'a';
+	// entry[1] = 'b';
+	// entry[2] = 'c';
 
+	char bob = '0';
+
+	while (strncmp(entry, end_entry, len_n) != 0 && b != 32) { // While entry not last entry
+
+		// This loop only applies if you have acc -> baa
 		for (int i = len_n - 1; i > -1; i--) { // Go back down the entry list from the back
 
 			if (entry[i] == last_elem) { // If the current entry index is the last elem
-				for (int j = i; j < len_n; j++)
-					if (entry[j] != last_elem)
-						goto NOT;
+
+				if (entry[i - 1] == last_elem)
+					continue;
+
+				// for (int j = i; j < len_n; j++) // Run over previous elements and check if they are also last_elem
+				// 	if (entry[j] != last_elem)
+				// 		goto NOT;
+
 				printf("%s\n", entry);
-				entry[i - 1] = get_next_char(entry[i - 1]);
+				bob = get_next_char(entry[i - 1]);
+				entry[i - 1] = bob;
+				// entry[i - 1] = get_next_char(entry[i - 1]);
 
 				for (int j = i; j < len_n; j++) // Reset current index and forward ones to base choice
 					entry[j] = choices[0];
-				NOT: break;
-			}
+				break;
+
+			} else
+				break;
 		}
 		printf("%s\n", entry);
-		entry[len_n - 1] = get_next_char(entry[len_n - 1]); // PROB here now, acc
+		bob = get_next_char(entry[len_n - 1]);
+		entry[len_n - 1] = bob;
+		// entry[len_n - 1] = get_next_char(entry[len_n - 1]); // PROB here now, acc
 		b++;
 	}
+	printf("%s\n", entry);
 
-	return 0;
+	return 1;
 }
 
 /*Print, then check?*/
