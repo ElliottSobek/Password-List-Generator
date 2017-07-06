170982cda2aeab99f9d31197756d3b7f89ed37e6
Elliott Sobek <sobeke@mymacewan.ca>
2017-07-06 01:52:35 -0600

    Implement inital character choice set logic

--- pwd-list-gen.c
+++ pwd-list-gen.c
@@ -4,9 +4,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-char get_next_char(const char c) {
-	const char *const choices = "abc";
+char get_next_char(const char c, const char *const choices) {
 	const int len_n = strlen(choices);
 
 	for (int i = 0; i < len_n; i++)
@@ -16,13 +16,20 @@ char get_next_char(const char c) {
 }
 
 int main(const int argc, char *const argv[]) {
+	const char num[11] = "0123456789",
+	lower[27] = "abcdefghijklmnopqrstuvwxyz",
+	upper[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	symbol[34] = "`~!@#$%%^&*()-_=+[]\\{}|;':\",./<>? ";
+	char choicess[96] = "0123456";
 
-	if (argc < 2) {
+	if ((argc < 2) || (argc > 8)) {
 		printf("Usage: ./a [-he] [-c Char set] [-p num] <filename>\n");
 		exit(EXIT_FAILURE);
 	}
 
 	int opt;
+	int entry_len = 8;
+	bool from_zero = false;
 
 	while ((opt = getopt(argc, argv, "hp:c:")) != -1) {
 		switch (opt) {
@@ -36,23 +43,73 @@ int main(const int argc, char *const argv[]) {
 				"\t-v\tChoose file type to .csv\n");
 			break;
 		case 'p':
+			entry_len = optind;
+			break;
+		case 'a':
+			from_zero = true;
 			break;
 		case 'c':
+			strncpy(choicess, "", 1);
+			switch (optarg[0]) {
+			case 'n':
+				strncat(choicess, num, 10);
+				break;
+			case 'u':
+				strncat(choicess, upper, 26);
+				break;
+			case 'l':
+				strncat(choicess, lower, 26);
+				break;
+			case 'p':
+				strncat(choicess, upper, 26);
+				strncat(choicess, lower, 26);
+				break;
+			case 'a':
+				strncat(choicess, num, 10);
+				strncat(choicess, upper, 26);
+				strncat(choicess, lower, 26);
+				break;
+			case 'w':
+				strncat(choicess, num, 10);
+				strncat(choicess, lower, 26);
+				break;
+			case 'e':
+				strncat(choicess, num, 10);
+				strncat(choicess, upper, 26);
+				break;
+			case 's':
+				strncat(choicess, num, 10);
+				strncat(choicess, upper, 26);
+				strncat(choicess, lower, 26);
+				strncat(choicess, symbol, 33);
+				break;
+			default:
+				fprintf(stderr, "Error. Unrecognized character set option.\n");
+				exit(EXIT_FAILURE);
+			}
 			break;
 		default:
-			fprintf(stderr, "Error\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 
 	FILE *fp = fopen(argv[argc - 1], "w");
 
-	const char *const choices = "abc";
+	// Start code section?
+
+	const char *const choices = strncpy(choicess, choicess, strlen(choicess));
 	const int len_n = strlen(choices);
 	const char last_elem = choices[len_n - 1];
-	char entry[4], end_entry[4];
+	char entry[entry_len + 1], end_entry[entry_len + 1];
 
-	for (int i = 0; i < len_n; i++) { // Init string/entry
+	// Clear the arrays
+	for (int i = 0; i < entry_len + 1; i++) {
+		entry[i] = '\0';
+		end_entry[i] = '\0';
+	}
+
+	// Init string/entry
+	for (int i = 0; i < len_n; i++) {
 		entry[i] = choices[0]; // "aaa"
 		end_entry[i] = last_elem; // "ccc"
 	}
@@ -69,7 +126,7 @@ int main(const int argc, char *const argv[]) {
 
 				fprintf(fp, "%s\n", entry);
 				printf("%s\n", entry);
-				entry[i - 1] = get_next_char(entry[i - 1]);
+				entry[i - 1] = get_next_char(entry[i - 1], choices);
 
 				for (int j = i; j < len_n; j++) // Reset current index and forward ones to base choice
 					entry[j] = choices[0];
@@ -80,10 +137,12 @@ int main(const int argc, char *const argv[]) {
 		}
 		fprintf(fp, "%s\n", entry);
 		printf("%s\n", entry);
-		entry[len_n - 1] = get_next_char(entry[len_n - 1]);
+		entry[len_n - 1] = get_next_char(entry[len_n - 1], choices);
 	}
 	fprintf(fp, "%s\n", entry);
 	printf("%s\n", entry);
+
+	// End code section?
 	fclose(fp);
 
 	return 1;
