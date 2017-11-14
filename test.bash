60a0fb8beb5b8883041b877c69d81e9e6bd52648
Elliott Sobek <sobeke@mymacewan.ca>
2017-11-14 00:48:50 -0700

    Add license header & shorted cut calls

--- test.bash
+++ test.bash
@@ -1,3 +1,14 @@
+# Tests aspects & behaviours about pwd-list-gen.
+# Copyright (C) 2017  Elliott Sobek
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 #!/bin/bash
 
 test () {
@@ -11,13 +22,20 @@ test () {
 	fi
 	echo "Pass file exe"
 
-	# Test file size calculation
+	# BEGIN TEST FILESIZE AND TOTAL ENTRY SIZE PRINT CALCULATION
+
+	# ./test.exe -g > test.txt
+	# cut -d ":" -f1 test.txt
+
+	# END TEST FILESIZE AND TOTAL ENTRY SIZE PRINT CALCULATION
+
+	# BEGIN TEST FILESIZE CALCULATION
 
 	# Single letter
 	./test.exe -q -l 1 -f t1.txt
 
 	local byte_size=$(du -b t1.txt | cut -f1)
-	local entries=$(wc -l t1.txt | cut --delimiter=" " -f1)
+	local entries=$(wc -l t1.txt | cut -d " " -f1)
 
 	if [ "$entries" != 10 -a "$byte_size" != 20 ]; then
 		echo "Fail single character"
@@ -29,7 +47,7 @@ test () {
 	./test.exe -q -l 4 -f t2.txt
 
 	byte_size=$(du -b t2.txt | cut -f1)
-	entries=$(wc -l t2.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t2.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 10000 -a "$byte_size" -ne 50000 ]; then
 		echo "Fail multicharacter"
@@ -41,7 +59,7 @@ test () {
 	./test.exe -aq -l 4 -f t3.txt
 
 	byte_size=$(du -b t3.txt | cut -f1)
-	entries=$(wc -l t3.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t3.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 11110 -a "$byte_size" -ne 55550 ]; then
 		echo "Fail all multicharacter"
@@ -53,7 +71,7 @@ test () {
 	./test.exe -q -L 2 -l 4 -f t4.txt
 
 	byte_size=$(du -b t4.txt | cut -f1)
-	entries=$(wc -l t4.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t4.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 11100 -a "$byte_size" -ne 54300 ]; then
 		echo "Fail set min"
@@ -65,7 +83,7 @@ test () {
 	./test.exe -q -l 4 -c l -f t6.txt
 
 	byte_size=$(du -b t6.txt | cut -f1)
-	entries=$(wc -l t6.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t6.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 456976 -a "$byte_size" -ne 2284880 ]; then
 		echo "Fail lower"
@@ -77,7 +95,7 @@ test () {
 	./test.exe -q -l 4 -c u -f t7.txt
 
 	byte_size=$(du -b t7.txt | cut -f1)
-	entries=$(wc -l t7.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t7.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 456976 -a "$byte_size" -ne 2284880 ]; then
 		echo "Fail upper"
@@ -89,7 +107,7 @@ test () {
 	./test.exe -q -l 3 -c p -f t8.txt
 
 	byte_size=$(du -b t8.txt | cut -f1)
-	entries=$(wc -l t8.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t8.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 140608 -a "$byte_size" -ne 562432 ]; then
 		echo "Fail alpha"
@@ -101,7 +119,7 @@ test () {
 	./test.exe -q -l 3 -c a -f t9.txt
 
 	byte_size=$(du -b t9.txt | cut -f1)
-	entries=$(wc -l t9.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t9.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 238328 -a "$byte_size" -ne 953312 ]; then
 		echo "Fail alnum"
@@ -113,7 +131,7 @@ test () {
 	./test.exe -q -l 3 -c s -f t10.txt
 
 	byte_size=$(du -b t10.txt | cut -f1)
-	entries=$(wc -l t10.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t10.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 857375 -a "$byte_size" -ne 3429500 ]; then
 		echo "Fail all choice set"
@@ -125,14 +143,17 @@ test () {
 	./test.exe -q -l 4 > t11.txt
 
 	byte_size=$(du -b t11.txt | cut -f1)
-	entries=$(wc -l t11.txt | cut --delimiter=" " -f1)
+	entries=$(wc -l t11.txt | cut -d " " -f1)
 
 	if [ "$entries" -ne 10000 -a "$byte_size" -ne 50000 ]; then
 		echo "Fail redirection"
 		return 1
 	fi
 	echo "Pass redirection"
-	echo "All tests PASSED"
+
+	# END TEST FILESIZE CALCULATION
+
+	echo "ALL TESTS PASSED"
 
 	rm *.o *.txt ./test.exe
 
